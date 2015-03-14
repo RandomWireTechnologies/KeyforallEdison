a77500067a05660be3d669ffc32eec83bb5a0f21
Stephen Wong <stephwwong@gmail.com>
2015-03-14 16:47:13 -0400

    Oops, should have used raw_input()

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ kwikset.setup_serial()
 
 kwikset.setup_arduinobreakout_pins()
 
-lock = input("What do you want to do? ")
+lock = raw_input("What do you want to do? ")
 
 if(lock == 'lock'):
     kwikset.lock()
