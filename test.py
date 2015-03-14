7310be6a5344b7bd3e336c1d57ce6906b245ae2a
Stephen Wong <stephwwong@gmail.com>
2015-03-14 16:45:38 -0400

    Fixed test script again

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ kwikset.setup_serial()
 
 kwikset.setup_arduinobreakout_pins()
 
-lock = input("What do you want to do?")
+lock = input("What do you want to do? ")
 
-if(lock == "lock"):
+if(lock == 'lock'):
     kwikset.lock()
 else:
     kwikset.unlock()
